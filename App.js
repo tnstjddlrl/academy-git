a3c5c53f6363bebe3acc70fd4e8f8180267c5e4a
tnstjddlrl <tnstjddlrl@naver.com>
2021-02-23 21:17:42 +0900

    인트로 작업전 커밋

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { InterstitialAd, RewardedAd,BannerAdSize, BannerAd, TestIds } from '@react-native-firebase/admob';
+import { InterstitialAd, RewardedAd, BannerAdSize, BannerAd, TestIds } from '@react-native-firebase/admob';
 import React from 'react';
 import {
   SafeAreaView,
@@ -7,15 +7,32 @@ import {
   View,
   Text,
   StatusBar,
+  Dimensions,
+  Image
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient'
 
-import Banner from './banner'
+import Banner from './source/banner'
 
-const App = ()=>{
-  return(
+
+const charwidth = Dimensions.get('window').width
+const charheight = Dimensions.get('window').height
+
+const codeimg = require('./img/code.png')
+
+
+const App = () => {
+  return (
     <View>
-      <Banner></Banner>
-      <Text>안녕</Text>
+      <LinearGradient colors={['#FFB484', '#ffbf80', '#ffd9b3']} style={{ width: charwidth, height: charheight }}>
+        <View>
+          <Image source={codeimg} style={{position:'absolute',maxWidth:300,maxHeight:300,marginTop:200}}></Image>
+          <View style={{ justifyContent: 'center', alignItems: 'center', height: charheight }}>
+            <Text style={{ fontSize: 70, textAlign: 'center', marginRight: 50,color:'white' }}>C & sCript</Text>
+            <Text style={{ fontSize: 50, textAlign: 'center', marginLeft: 100,color:'white' }}>aCademy</Text>
+          </View>
+        </View>
+      </LinearGradient>
     </View>
   )
 }
---
+++ source/introview.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+  } from 'react-native';
+
+  const IntroView = () =>{
+      return(
+        <View>
+            <Text>인트로</Text>
+        </View>
+      )
+  }
+
+  export default IntroView
