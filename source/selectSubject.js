071bbcaf145b0d2935ac3a32f101a26177ce5362
tnstjddlrl <tnstjddlrl@naver.com>
2021-03-07 22:44:18 +0900

    yes?

---
+++ source/selectSubject.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Dimensions,
+  Image
+} from 'react-native';
+
+const SelectSubj = () =>{
+    return(
+        <View>
+            <Text>선택화면</Text>
+        </View>
+    )
+}
+
+export default SelectSubj
