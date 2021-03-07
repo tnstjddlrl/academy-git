13749d50eabff6ba230753b6b913fa1591f16dfc
tnstjddlrl <tnstjddlrl@naver.com>
2021-03-07 23:07:06 +0900

    메인화면 수정완료

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import 'react-native-gesture-handler' ; 
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,
@@ -12,7 +12,7 @@ import {
   Image
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient'
-import {NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 import IntroView from './source/introview';
 
@@ -20,35 +20,39 @@ const Stack = createStackNavigator();
 import { useNavigation } from '@react-navigation/native';
 import { useEffect } from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import SelectSubj from './source/selectSubject';
 
 const charwidth = Dimensions.get('window').width
 const charheight = Dimensions.get('window').height
 
 const codeimg = require('./img/code.png')
-
+// const first = require('../img/first.png')
 
 
 const App = () => {
   const navigation = useNavigation()
 
-  useEffect(()=>{
+  useEffect(() => {
     setTimeout(() => {
-      navigation.navigate('인트로')  
+      navigation.navigate('인트로')
     }, 1000);
   })
 
   return (
     <View>
-      <TouchableOpacity onPress={()=>navigation.navigate('인트로')  }>
-      <LinearGradient colors={['#FFB484', '#ffbf80', '#ffd9b3']} style={{ width: charwidth, height: charheight }}>
-        <View>
-          <Image source={codeimg} style={{position:'absolute',maxWidth:300,maxHeight:300,marginTop:200}}></Image>
-          <View style={{ justifyContent: 'center', alignItems: 'center', height: charheight }}>
-            <Text style={{ fontSize: 70, textAlign: 'center', marginRight: 50,color:'white' }}>C & sCript</Text>
-            <Text style={{ fontSize: 50, textAlign: 'center', marginLeft: 100,color:'white' }}>aCademy</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('인트로')}>
+        <View style={{ width: charwidth, height: charheight }}>
+          <View style={{ flex: 1, alignItems: 'center',justifyContent:'flex-end' }}>
+          <Text style={{ fontFamily: 'DoHyeon', textAlign: 'center', fontSize: 50 }}>C&Script</Text>
+          <Text style={{ fontFamily: 'DoHyeon', textAlign: 'center', fontSize: 50 }}>Academy</Text>
           </View>
+          <View style={{ flex: 2, justifyContent:'flex-start', alignItems: 'center' }}>
+            <Image source={codeimg} style={{ maxWidth: charwidth - 20, maxHeight: charheight / 2.2 }}></Image>
+          </View>
+          <View style={{ flex: 1, justifyContent:'flex-start', alignItems: 'center' }}>
+          </View>
+
         </View>
-      </LinearGradient>
       </TouchableOpacity>
     </View>
   )
@@ -57,10 +61,11 @@ const App = () => {
 const Navi = () => {
   return (
     <NavigationContainer>
-    <Stack.Navigator headerMode={'none'}>
-      <Stack.Screen name="App" component={App} />
-      <Stack.Screen name="인트로" component={IntroView} />
-    </Stack.Navigator>
+      <Stack.Navigator headerMode={'none'}>
+        <Stack.Screen name="App" component={App} />
+        <Stack.Screen name="인트로" component={IntroView} />
+        <Stack.Screen name="선택화면" component={SelectSubj} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
--- source/introview.js
+++ source/introview.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import {
     SafeAreaView,
@@ -8,7 +9,9 @@ import {
     StatusBar,
     Dimensions,
     Image,
+    TouchableWithoutFeedback,
   } from 'react-native';
+import Navi from '../App';
   
   const charwidth = Dimensions.get('window').width
   const charheight = Dimensions.get('window').height
@@ -20,6 +23,7 @@ import {
 
 
   const IntroView = () =>{
+    const navigation = useNavigation()
       return(
         <View>
           <ScrollView horizontal={true} pagingEnabled={true} showsHorizontalScrollIndicator={false}>
@@ -33,7 +37,7 @@ import {
               <View style={{flex:2,alignItems:'center'}}>
 
               <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:40}}>쉬운 문제로 구성된 커리큘럼</Text>
-              <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:25,color:'gray',marginTop:20,margin:10}}>코딩을 쉽게 접근하기 위해서 매우 간단하고 단단한 기본기를 만들어줄거에요!</Text>
+              <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:25,color:'gray',marginTop:20,margin:10,lineHeight:30}}>코딩을 쉽게 접근하기 위해서 매우 간단하고 단단한 기본기를 만들어줄거에요!</Text>
               </View>
             </View>
             {/*  */}
@@ -47,7 +51,7 @@ import {
               <View style={{flex:2,alignItems:'center'}}>
 
               <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:40}}>논리적 사고를 기르게 해주는 문제들</Text>
-              <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:25,color:'gray',marginTop:20,margin:10}}>프로그래밍 언어를 단지 도구라고 생각하게 함으로써 다른 사람의 힘이 아닌 본인의 머릿속에서 창의적인 코드를 만들 수 있 도와줄 거에요!</Text>
+              <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:25,color:'gray',marginTop:20,margin:10,lineHeight:30}}>프로그래밍 언어를 단지 도구라고 생각하게 함으로써 다른 사람의 힘이 아닌 본인의 머릿속에서 창의적인 코드를 만들 수 있 도와줄 거에요!</Text>
               </View>
             </View>
             {/*  */}
@@ -61,12 +65,13 @@ import {
               <View style={{flex:2,alignItems:'center'}}>
 
               <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:40}}>폭넓은 언어 사용 유도</Text>
-              <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:25,color:'gray',marginTop:20,margin:10}}>젓가락질에 익숙해지면 종류에 상관없이 젓가락질을 할 수 있듯 코딩도 익숙해지면 폭넓게 사용할 수 있습니다. 처음은 어렵겠지만 차근차근 따라오면 어느새 여러분도 코딩 마스터!</Text>
+              <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:25,color:'gray',marginTop:20,margin:10,lineHeight:30}}>젓가락질에 익숙해지면 종류에 상관없이 젓가락질을 할 수 있듯 코딩도 익숙해지면 폭넓게 사용할 수 있습니다. 처음은 어렵겠지만 차근차근 따라오면 어느새 여러분도 코딩 마스터!</Text>
               </View>
             </View>
             {/*  */}
 
             {/*  */}
+            <TouchableWithoutFeedback onPress={()=>{navigation.navigate('선택화면')}}>
             <View style={{width:charwidth,height:charheight}}>
               <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
                 <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:40}}>앞의 내용은 잘 읽으셨나요?</Text>
@@ -78,23 +83,13 @@ import {
               <View style={{flex:2,alignItems:'center'}}>
 
               <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:40,margin:5}}>따라올 준비가 되었다면 눌러주세요!</Text>
-              
+              <Text style={{fontFamily:'DoHyeon',textAlign:'center',fontSize:25,color:'gray',marginTop:20,margin:10}}>화면을 클릭해주세요</Text>
               </View>
             </View>
+            </TouchableWithoutFeedback>
             {/*  */}
 
 
-            <View style={{width:charwidth,height:charheight,backgroundColor:'yellow'}}>
-              <Text>마지막페이지</Text>
-              <View style={{alignContent:'flex-end',flex:1}}>
-                <View style={{width:charwidth,height:50,backgroundColor:'red'}}><Text>시작!</Text></View>
-              </View>
-
-
-            </View>
-
-
-            <Text>인트로 바뀜</Text>
           </ScrollView>
             
         </View>
